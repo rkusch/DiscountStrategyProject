3b0b64efd01d03048981a2c73facfebe868ea4f1
ryan <ryan@macbook-pro>
2017-10-12 06:53:12 -0500

    adding properties and methods to each class

--- src/discountstrategyproject/Customer.java
+++ src/discountstrategyproject/Customer.java
@@ -10,5 +10,31 @@ package discountstrategyproject;
  * @author rkusch
  */
 public class Customer {
+    private String customerID;
+    //would have liked to break this down into first and last name
+    private String customerFullName;
+
+    public final String getCustomerID() {
+        return customerID;
+    }
+
+    public final void setCustomerID(String customerID) {
+        this.customerID = customerID;
+    }
+
+    public final String getCustomerFullName() {
+        return customerFullName;
+    }
+
+    public final void setCustomerFullName(String customerFullName) {
+        this.customerFullName = customerFullName;
+    }
+
+    public Customer(String customerID, String customerFullName) {
+        setCustomerID(customerID);
+        setCustomerFullName(customerFullName);
+    }
+    
+    
     
 }
